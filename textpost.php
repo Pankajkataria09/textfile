7924e9035a31f0d674605106b7e966f3554fefcb
Pankajkataria09 <pankajkataria09@gmail.com>
2023-01-20 15:52:19 +0530

    add new file

---
+++ textpost.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="" href="register.css">
+    <title>Document</title>
+    <style></style>
+   
+</head>
+<body>
+    <?php
+    echo "<h1>"." welcome"." </h1>";
+    ?>
+        <form action="" method="GET">
+        <fieldset></fieldset> 
+
+        
+
+        <label>SUBMIT YOUR POST </label>
+
+        <input class="tp" type="text" name="content" id="" placeholder="" >
+        <br>
+        <div class="division">
+        <input class="btn" type="submit" value="submit" name="submit">
+        </div >
+      
+        </form>
+     
+            
+</body>
+</html>
+
+
